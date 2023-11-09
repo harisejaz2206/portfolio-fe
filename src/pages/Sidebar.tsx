fd60246c00b33efbb6bb51185c9cbb4b979fcd4e
Burhan Saqib <burhan.sq4906@gmail.com>
2023-11-09 17:30:51 +0500

    Fixed SideBar spacing

--- src/pages/Sidebar.tsx
+++ src/pages/Sidebar.tsx
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // import { FaHome, FaStore, FaChartBar, FaLifeRing, FaCog, FaUsers, FaSignOutAlt, FaCaretDown, FaCaretRight } from 'react-icons/fa';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, Link, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, Link, useLocation } from "react-router-dom";
 import { AppThunkDispatch } from "../store/rootReducer";
-import { logout } from '../app/features/auth/auth.slice';
-import { clearOutletData } from "../app/features/outlet/outlet.slice"
+import { logout } from "../app/features/auth/auth.slice";
+import { clearOutletData } from "../app/features/outlet/outlet.slice";
 // import { Link } from 'react-router-dom';
-import { FaHome, FaStore, FaChartBar, FaBook, FaInbox, FaStar, FaShoppingCart, FaCog, FaUsers, FaSignOutAlt, FaCaretDown, FaCaretRight, FaEnvelope, FaUser } from 'react-icons/fa';
-import { clearStoreData } from '../app/features/store/store.slice';
-import { selectUser } from '../app/features/auth/auth.selector';
+import {
+  FaHome,
+  FaStore,
+  FaChartBar,
+  FaBook,
+  FaInbox,
+  FaStar,
+  FaShoppingCart,
+  FaCog,
+  FaUsers,
+  FaSignOutAlt,
+  FaCaretDown,
+  FaCaretRight,
+  FaEnvelope,
+  FaUser,
+} from "react-icons/fa";
+import { clearStoreData } from "../app/features/store/store.slice";
+import { selectUser } from "../app/features/auth/auth.selector";
 
 interface SidebarItemProps {
   icon: JSX.Element;
@@ -22,7 +37,10 @@ interface SidebarSubItemProps {
   isActive: boolean;
 }
 
-const UserProfileCard: React.FC<{ userIcon: JSX.Element, userEmail: string }> = ({ userIcon, userEmail }) => {
+const UserProfileCard: React.FC<{
+  userIcon: JSX.Element;
+  userEmail: string;
+}> = ({ userIcon, userEmail }) => {
   return (
     <div className="bg-white p-2 ">
       <div className="flex flex-col items-center space-y-2">
@@ -44,7 +62,7 @@ const Sidebar: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const user = useSelector(selectUser);
-  const userEmail = user?.email || ""; 
+  const userEmail = user?.email || "";
 
   const toggleInventory = () => {
     setInventoryOpen(!isInventoryOpen);
@@ -52,115 +70,128 @@ const Sidebar: React.FC = () => {
 
   const handleLogout = () => {
     dispatch(logout());
-    dispatch(clearOutletData())
-    dispatch(clearStoreData())
-    navigate('/');
+    dispatch(clearOutletData());
+    dispatch(clearStoreData());
+    navigate("/");
   };
 
   return (
-    <div className="bg-white h-auto w-2/3 p-4 md:w-1/6 flex flex-col justify-between">
-      <UserProfileCard userIcon={<FaUser/>} userEmail={userEmail} /> 
-          <Link to={"/multi-admin/"}>
-            <SidebarItem
-              icon={<FaHome />}
-              text="Dashboard"
-              isActive={location.pathname === '/multi-admin/'}
-            />
-          </Link>
-          <Link to={"/multi-admin/outlets"}>
-            <SidebarItem
-              icon={<FaStore />}
-              text="Outlets" to="/outlets"
-              isActive={location.pathname === '/multi-admin/outlets'}
-            />
-          </Link>
-          <div className="group flex items-center justify-between py-2 px-2 mt-4 text-gray-600 cursor-pointer transition-bg hover:bg-indigo-500 hover:rounded-md hover:text-white " onClick={toggleInventory}>
-            <FaChartBar />
-            <span className="text-xs -ml-10">Inventory</span>
-            {isInventoryOpen ? <FaCaretDown className="ml-1" /> : <FaCaretRight className="ml-1" />}
-          </div>
-          {isInventoryOpen && (
-            <div className="pl-6">
-              {/* <Link to="/multi-admin/products">
+    <div className="bg-white h-screen w-2/3 p-4 md:w-1/6 flex flex-col justify-between">
+      <UserProfileCard userIcon={<FaUser />} userEmail={userEmail} />
+      <Link to={"/multi-admin/"}>
+        <SidebarItem
+          icon={<FaHome />}
+          text="Dashboard"
+          isActive={location.pathname === "/multi-admin/"}
+        />
+      </Link>
+      <Link to={"/multi-admin/outlets"}>
+        <SidebarItem
+          icon={<FaStore />}
+          text="Outlets"
+          to="/outlets"
+          isActive={location.pathname === "/multi-admin/outlets"}
+        />
+      </Link>
+      <div
+        className="group flex items-center justify-between py-2 px-2 mt-4 text-gray-600 cursor-pointer transition-bg hover:bg-indigo-500 hover:rounded-md hover:text-white "
+        onClick={toggleInventory}
+      >
+        <FaChartBar />
+        <span className="text-xs -ml-14">Inventory</span>
+        {isInventoryOpen ? (
+          <FaCaretDown className="ml-1" />
+        ) : (
+          <FaCaretRight className="ml-1" />
+        )}
+      </div>
+      {isInventoryOpen && (
+        <div className="pl-6">
+          {/* <Link to="/multi-admin/products">
                 <SidebarSubItem
                   text="Products"
                   isActive={location.pathname === '/multi-admin/products'}
                 />
           </Link> */}
-              <Link to="/multi-admin/catalogue">
-                <SidebarSubItem
-                  text="Catalogue"
-                  isActive={location.pathname === 'multi-admin/catalogue'}
-                />
-              </Link>
-              <Link to="/multi-admin/categories">
-                <SidebarSubItem
-                  text="Categories"
-                  isActive={location.pathname === '/multi-admin/categories'}
-                />
-              </Link>
-              <Link to="/multi-admin/brands">
-
-                <SidebarSubItem
-                  text="Brands"
-                  isActive={location.pathname === '/multi-admin/brands'}
-                />
-              </Link>
-            </div>
-          )}
-          <Link to={"/multi-admin/users"}>
-            <SidebarItem
-              icon={<FaUsers />}
-              text="Users"
-              isActive={location.pathname === '/multi-admin/users'} />
-          </Link>
-          <Link to={"/multi-admin/orders"}>
-            <SidebarItem
-              icon={<FaShoppingCart />}
-              text="Orders"
-              isActive={location.pathname === '/multi-admin/orders'}
+          <Link to="/multi-admin/catalogue">
+            <SidebarSubItem
+              text="Catalogue"
+              isActive={location.pathname === "multi-admin/catalogue"}
             />
           </Link>
-          <Link to={"/multi-admin/banners"}>
-            <SidebarItem
-              icon={<FaStar />}
-              text="Banners"
-              isActive={location.pathname === '/multi-admin/banners'}
+          <Link to="/multi-admin/categories">
+            <SidebarSubItem
+              text="Categories"
+              isActive={location.pathname === "/multi-admin/categories"}
             />
           </Link>
-          <SidebarItem
-            icon={<FaBook />}
-            text="Pages"
-            isActive={location.pathname === ''}
-          />
-          <Link to={"/multi-admin/chat"}>
-            <SidebarItem
-              icon={<FaInbox />}
-              text="Chat"
-              isActive={location.pathname === '/multi-admin/chat'}
+          <Link to="/multi-admin/brands">
+            <SidebarSubItem
+              text="Brands"
+              isActive={location.pathname === "/multi-admin/brands"}
             />
           </Link>
-          <SidebarItem
-            icon={<FaCog />}
-            text="Settings"
-            isActive={location.pathname === ''}
-          />
-          <div>
-            <button className="flex text-xs items-center text-white cursor-pointer bg-red-600 rounded-md py-2 px-2 w-40 mt-[10%]" onClick={handleLogout}>
-              <FaSignOutAlt className="mr-2" />
-              Log Out
-            </button>
-          </div>
-        
-      
-
+        </div>
+      )}
+      <Link to={"/multi-admin/users"}>
+        <SidebarItem
+          icon={<FaUsers />}
+          text="Users"
+          isActive={location.pathname === "/multi-admin/users"}
+        />
+      </Link>
+      <Link to={"/multi-admin/orders"}>
+        <SidebarItem
+          icon={<FaShoppingCart />}
+          text="Orders"
+          isActive={location.pathname === "/multi-admin/orders"}
+        />
+      </Link>
+      <Link to={"/multi-admin/banners"}>
+        <SidebarItem
+          icon={<FaStar />}
+          text="Banners"
+          isActive={location.pathname === "/multi-admin/banners"}
+        />
+      </Link>
+      <SidebarItem
+        icon={<FaBook />}
+        text="Pages"
+        isActive={location.pathname === ""}
+      />
+      <Link to={"/multi-admin/chat"}>
+        <SidebarItem
+          icon={<FaInbox />}
+          text="Chat"
+          isActive={location.pathname === "/multi-admin/chat"}
+        />
+      </Link>
+      <SidebarItem
+        icon={<FaCog />}
+        text="Settings"
+        isActive={location.pathname === ""}
+      />
+      <div>
+        <button
+          className="flex text-xs items-center text-white cursor-pointer bg-red-600 rounded-md py-2 px-2 w-40 mt-[10%]"
+          onClick={handleLogout}
+        >
+          <FaSignOutAlt className="mr-2" />
+          Log Out
+        </button>
+      </div>
     </div>
   );
 };
 
 const SidebarItem: React.FC<SidebarItemProps> = ({ icon, text, isActive }) => {
   return (
-    <div className={`group flex items-center py-2 px-2 mt-4 text-gray-600 cursor-pointer transition-bg rounded-md hover:bg-indigo-500 hover:rounded-md hover:text-white ${isActive ? 'bg-indigo-500 text-white' : ''}`} title={text}>
+    <div
+      className={`group flex items-center py-2 px-2 mt-4 text-gray-600 cursor-pointer transition-bg rounded-md hover:bg-indigo-500 hover:rounded-md hover:text-white ${
+        isActive ? "bg-indigo-500 text-white" : ""
+      }`}
+      title={text}
+    >
       {icon}
       <span className="ml-2 text-xs">{text}</span>
     </div>
@@ -169,7 +200,12 @@ const SidebarItem: React.FC<SidebarItemProps> = ({ icon, text, isActive }) => {
 
 const SidebarSubItem: React.FC<SidebarSubItemProps> = ({ text, isActive }) => {
   return (
-    <div className={`group flex items-center py-2 px-2 mt-2 text-gray-600 cursor-pointer transition-bg rounded-md hover:bg-indigo-500 hover:rounded-md hover:text-white ${isActive ? 'bg-indigo-500 text-white' : ''}`} title={text}>
+    <div
+      className={`group flex items-center py-2 px-2 mt-2 text-gray-600 cursor-pointer transition-bg rounded-md hover:bg-indigo-500 hover:rounded-md hover:text-white ${
+        isActive ? "bg-indigo-500 text-white" : ""
+      }`}
+      title={text}
+    >
       <span className="text-xs">{text}</span>
     </div>
   );
--- src/pages/SoleChainSidebar.tsx
+++ src/pages/SoleChainSidebar.tsx
@@ -18,7 +18,7 @@ import {
 } from "react-icons/fa";
 import { GrUserAdmin } from "react-icons/gr";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link, useLocation } from "react-router-dom";
 import { AppThunkDispatch } from "../store/rootReducer";
 import { logout } from "../app/features/auth/auth.slice";
 import { clearOutletData } from "../app/features/outlet/outlet.slice";
@@ -27,11 +27,13 @@ import { selectUser } from "../app/features/auth/auth.selector";
 interface SoleChainSidebarItemProps {
   icon: JSX.Element;
   text: string;
+  isActive: boolean;
   to?: string;
 }
 
 interface SoleChainSidebarSubItemProps {
   text: string;
+  isActive: boolean;
 }
 
 const UserProfileCard: React.FC<{
@@ -57,8 +59,10 @@ const SoleChainSidebar: React.FC = () => {
   const [isInventoryOpen, setInventoryOpen] = useState(false);
   const dispatch = useDispatch<AppThunkDispatch>();
   const navigate = useNavigate();
+  const location = useLocation();
   const user = useSelector(selectUser);
-  const userEmail = user?.email || ""; // Provide a default value if userEmail is undefined
+  const userEmail = user?.email || "";
+
   const toggleInventory = () => {
     setInventoryOpen(!isInventoryOpen);
   };
@@ -70,13 +74,17 @@ const SoleChainSidebar: React.FC = () => {
   };
 
   return (
-    <div className="bg-white h-auto w-1/5 p-4 md:w-1/6 flex flex-col justify-between">
+    <div className="bg-white h-screen w-1/5 p-4 md:w-1/6 flex flex-col justify-between">
       <UserProfileCard userIcon={<FaUser />} userEmail={userEmail} />
       <Link to={"/sole-admin/"}>
-        <SoleChainSidebarItem icon={<FaHome />} text="Dashboard" />
+        <SoleChainSidebarItem
+          icon={<FaHome />}
+          text="Dashboard"
+          isActive={location.pathname === "/sole-admin/"}
+        />
       </Link>
       <div
-        className="group flex items-center justify-between py-2 px-2 mt-4 text-gray-600 cursor-pointer transition-bg hover:bg-orange-500 hover:rounded-md hover:text-white "
+        className="group flex items-center justify-between py-2 px-2 mt-4 text-gray-600 cursor-pointer transition-bg hover:bg-indigo-500 hover:rounded-md hover:text-white "
         onClick={toggleInventory}
       >
         <FaChartBar />
@@ -90,32 +98,60 @@ const SoleChainSidebar: React.FC = () => {
       {isInventoryOpen && (
         <div className="pl-6">
           <Link to="/sole-admin/products">
-            <SoleChainSidebarSubItem text="Products" />
+            <SoleChainSidebarSubItem
+              text="Products"
+              isActive={location.pathname === "/sole-admin/products"}
+            />
           </Link>
           <Link to="/sole-admin/catalogue">
-            <SoleChainSidebarSubItem text="Catalogue" />
+            <SoleChainSidebarSubItem
+              text="Catalogue"
+              isActive={location.pathname === "/sole-admin/catalogue"}
+            />
           </Link>
           <Link to="/sole-admin/brands">
-            <SoleChainSidebarSubItem text="Brands" />
+            <SoleChainSidebarSubItem
+              text="Brands"
+              isActive={location.pathname === "/sole-admin/brands"}
+            />
           </Link>
           <Link to="/sole-admin/categories">
-            <SoleChainSidebarSubItem text="Categories" />
+            <SoleChainSidebarSubItem
+              text="Categories"
+              isActive={location.pathname === "/sole-admin/categories"}
+            />
           </Link>
         </div>
       )}
       <Link to={"/sole-admin/users"}>
-        <SoleChainSidebarItem icon={<FaUsers />} text="Users" />
+        <SoleChainSidebarItem
+          icon={<FaUsers />}
+          text="Users"
+          isActive={location.pathname === "/sole-admin/users"}
+        />
       </Link>
       <Link to={"/sole-admin/orders"}>
-        <SoleChainSidebarItem icon={<FaShoppingCart />} text="Orders" />
+        <SoleChainSidebarItem
+          icon={<FaShoppingCart />}
+          text="Orders"
+          isActive={location.pathname === "/sole-admin/orders"}
+        />
       </Link>
       <Link to={"/sole-admin/banners"}>
-        <SoleChainSidebarItem icon={<FaImages />} text="Banners" />
+        <SoleChainSidebarItem
+          icon={<FaImages />}
+          text="Banners"
+          isActive={location.pathname === "/sole-admin/banners"}
+        />
       </Link>
-      <SoleChainSidebarItem icon={<FaCog />} text="Settings" />
+      <SoleChainSidebarItem
+        icon={<FaCog />}
+        text="Settings"
+        isActive={location.pathname === ""}
+      />
       <div>
         <button
-          className="flex text-xs items-center text-white cursor-pointer bg-red-600 rounded-md py-2 px-2 w-40 mt-4"
+          className="flex text-xs items-center text-white cursor-pointer bg-red-600 rounded-md py-2 px-2 w-40 mt-[10%]"
           onClick={handleLogout}
         >
           <FaSignOutAlt className="mr-2" />
@@ -129,10 +165,13 @@ const SoleChainSidebar: React.FC = () => {
 const SoleChainSidebarItem: React.FC<SoleChainSidebarItemProps> = ({
   icon,
   text,
+  isActive,
 }) => {
   return (
     <div
-      className="group flex items-center py-2 px-2 mt-4 text-gray-600 cursor-pointer transition-bg hover:bg-orange-500 hover:rounded-md hover:text-white "
+      className={`group flex items-center py-2 px-2 mt-4 text-gray-600 cursor-pointer transition-bg rounded-md hover:bg-indigo-500 hover:rounded-md hover:text-white ${
+        isActive ? "bg-indigo-500 text-white" : ""
+      }`}
       title={text}
     >
       {icon}
@@ -143,9 +182,15 @@ const SoleChainSidebarItem: React.FC<SoleChainSidebarItemProps> = ({
 
 const SoleChainSidebarSubItem: React.FC<SoleChainSidebarSubItemProps> = ({
   text,
+  isActive,
 }) => {
   return (
-    <div className="group flex items-center py-1 pl-2 mt-1 text-gray-600 cursor-pointer transition-bg hover:bg-orange-500 hover:rounded-md hover:text-white">
+    <div
+      className={`group flex items-center py-2 px-2 mt-2 text-gray-600 cursor-pointer transition-bg rounded-md hover:bg-indigo-500 hover:rounded-md hover:text-white ${
+        isActive ? "bg-indigo-500 text-white" : ""
+      }`}
+      title={text}
+    >
       <span className="text-xs">{text}</span>
     </div>
   );
